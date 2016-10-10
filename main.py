a4090d0a867a72fa7426ae7156bdb44c4c574b43
BuildTools <unconfigured@null.spigotmc.org>
2016-10-11 00:36:11 +0200

    ASDF

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import cortical
 import util
+import numpy as np
 
 def main():
     print("hello")
+    sgen = util.InputGenerator((3, 3),np.array([[0,1],[1,0]]),[0.2,0.7])
+    print(sgen.getInput(count=2))
 
 if __name__ == "__main__":
     main()
--- util.py
+++ util.py
@@ -1,3 +1,17 @@
+import numpy as np
+
 class InputGenerator():
-    def __init__(self, shape, rates):
+    def __init__(self, shape, signals, rates = 1):
         self.shape = shape
+        self.signals = signals
+        self.rates = rates
+        self.binRates = np.cumsum(rates/np.sum(rates))
+
+    def getInput(self, count=1):
+        return self.getSignal(count=count)
+
+    def getSignal(self, count=1, ind=None):
+        if ind == None:
+            return self.signals[np.digitize(np.random.rand(count),self.binRates)]
+        else:
+            return self.signals[ind]
