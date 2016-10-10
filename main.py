5409911fc6066889e0662b94276e2ee340149c9a
BuildTools <unconfigured@null.spigotmc.org>
2016-10-10 23:59:35 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import cortical
+import util
+
+def main():
+    print("hello")
+
+if __name__ == "__main__":
+    main()
---
+++ util.py
@@ -0,0 +1,3 @@
+class InputGenerator():
+    def __init__(self, shape, rates):
+        self.shape = shape
